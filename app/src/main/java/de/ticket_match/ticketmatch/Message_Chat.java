63c05b48edb59ae61c131fcfeadba486ad4e395c
Smux <p.reiffert@web.de>
2016-06-12 00:15:32 +0200

    Further development of the chat

--- app/src/main/java/de/ticket_match/ticketmatch/Message_Chat.java
+++ app/src/main/java/de/ticket_match/ticketmatch/Message_Chat.java
@@ -1,244 +1,173 @@
 package de.ticket_match.ticketmatch;
 
-import android.os.Build;
+import android.graphics.Color;
+
+import android.app.ListActivity;
+import android.content.SharedPreferences;
+import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-
-import de.ticket_match.ticketmatch.entities.Message;
-
-public class Message_Chat extends AppCompatActivity implements View.OnClickListener, MessageDataSource.MessagesCallbacks{
-
-    public static final String USER_EXTRA = "USER";
-
-    public static final String TAG = "ChatActivity";
-
-    private ArrayList<Message> mMessages;
-    private MessagesAdapter mAdapter;
-    private String mRecipient;
-    private ListView mListView;
-    private Date mLastMessageDate = new Date();
-    private String mConvoId;
-    private MessageDataSource.MessagesListener mListener;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message__chat);
-
-        mRecipient = "Ashok";
-
-        mListView = (ListView)findViewById(R.id.message_chat);
-        mMessages = new ArrayList<>();
-        mAdapter = new MessagesAdapter(mMessages);
-        mListView.setAdapter(mAdapter);
-
-        //TODO: Set title of chat name
-        /*setTitle(mRecipient);
-        if (getSupportActionBar() != null){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }*/
-
-        Button sendMessage = (Button)findViewById(R.id.send_message);
-        sendMessage.setOnClickListener(this);
-
-        String[] ids = {"Ajay","-", "Ashok"};
-        Arrays.sort(ids);
-        mConvoId = ids[0]+ids[1]+ids[2];
-
-
-        //TODO: Change from convoId to messages and then convoId
-        mListener = MessageDataSource.addMessagesListener(mConvoId, this);
-    }
-
-    public void onClick(View v) {
-        EditText newMessageView = (EditText)findViewById(R.id.new_chat_message);
-        String newMessage = newMessageView.getText().toString();
-        newMessageView.setText("");
-        Message msg = new Message();
-        msg.setDate(new Date());
-        msg.setText(newMessage);
-        msg.setSender("Ashok");
-
-        MessageDataSource.saveMessage(msg, mConvoId);
-    }
-
-    @Override
-    public void onMessageAdded(Message message) {
-        mMessages.add(message);
-        mAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        MessageDataSource.stop(mListener);
-    }
-
-
-    private class MessagesAdapter extends ArrayAdapter<Message> {
-        MessagesAdapter(ArrayList<Message> messages){
-            super(Message_Chat.this, R.layout.message, R.id.message, messages);
-        }
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = super.getView(position, convertView, parent);
-            Message message = getItem(position);
-
-            TextView nameView = (TextView)convertView.findViewById(R.id.message);
-            nameView.setText(message.getText());
-
-            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams)nameView.getLayoutParams();
-
-            int sdk = Build.VERSION.SDK_INT;
-            if (message.getSender().equals("Ashok")){
-                if (sdk >= Build.VERSION_CODES.JELLY_BEAN) {
-                    nameView.setBackground(getDrawable(R.drawable.bubble_right_green));
-                } else{
-                    nameView.setBackgroundDrawable(getDrawable(R.drawable.bubble_right_green));
-                }
-                layoutParams.gravity = Gravity.RIGHT;
-                //nameView.setGravity(Gravity.RIGHT);
-
-            }else{
-                if (sdk >= Build.VERSION_CODES.JELLY_BEAN) {
-                    nameView.setBackground(getDrawable(R.drawable.bubble_left_gray));
-                } else{
-                    nameView.setBackgroundDrawable(getDrawable(R.drawable.bubble_left_gray));
-                }
-                layoutParams.gravity = Gravity.LEFT;
-                //nameView.setGravity(Gravity.LEFT);
-            }
-
-            nameView.setLayoutParams(layoutParams);
-
-
-            return convertView;
-        }
-    }
-}
-
-/*import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
+
+import java.security.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Random;
 
+import de.ticket_match.ticketmatch.entities.Message;
 
-public class Message_Chat extends AppCompatActivity {
+public class Message_Chat extends ListActivity {
+    // TODO: change this to your own Firebase URL
+    private static final String FIREBASE_URL = "https://project-6427965941977376836.firebaseio.com/";
 
+    private String mUsername;
+    private Firebase mFirebaseRef;
+    private ValueEventListener mConnectedListener;
+    private ChatListAdapter mChatListAdapter;
 
-
-    ArrayList<String> chatmessages = new ArrayList<String>(0);
-    String name = "";
+    private String currentKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message__chat);
 
-        Bundle bund = ((MainActivityTabHost)getParent()).baseBundle;
-        name = bund.getString("message_name");
+        // Make sure we have a mUsername
+        setupUsername();
 
-        ((TextView) findViewById(R.id.message_chat_name)).setText(name);
+        setTitle("Chatting as " + mUsername);
 
-        chatmessages.add("me|25.12.2016|Hello Nils!");
-        chatmessages.add("Nils Z.|26.01.2017|Hello Fred!");
+        // Setup our Firebase mFirebaseRef
+        mFirebaseRef = new Firebase(FIREBASE_URL).child("chats");
 
-        ((ListView) findViewById(R.id.message_chat)).setAdapter(new CustomAdapter(this, chatmessages, name));
-
-    }
-
-    public void btn_sendMessage (View view) {
-        String message_text = ((EditText)findViewById(R.id.new_chat_message)).getText().toString();
-        if (message_text.equals("")) {
-            Toast.makeText(getApplicationContext(),"Please fill out the requiered information!",Toast.LENGTH_SHORT).show();
-        } else {
-            String chat = "me|" + new SimpleDateFormat("dd.MM.yyyy").format(new Date()) + "|" + message_text;
-            chatmessages.add(chat);
-            ((CustomAdapter)((ListView) findViewById(R.id.message_chat)).getAdapter()).notifyDataSetChanged();
-            ((EditText)findViewById(R.id.new_chat_message)).setText("");
-        }
-    }
-
-    public static class CustomAdapter extends BaseAdapter {
-        ArrayList<String> result;
-        String sname;
-        Context context;
-        private static LayoutInflater inflater=null;
-
-        public CustomAdapter(Message_Chat mainActivity, ArrayList<String>  prgmNameList, String name) {
-            result=prgmNameList;
-            sname = name;
-            context=mainActivity;
-            inflater = ( LayoutInflater )context.
-                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-        @Override
-        public int getCount() {
-            return result.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            String text = result.get(position);
-            String sender = text.substring(0, text.indexOf("|"));
-            String sdate = text.substring(text.indexOf("|")+1,text.indexOf("|",text.indexOf("|")+1));
-            String smessage = text.substring(text.indexOf("|",text.indexOf("|")+1)+1,text.length());
-
-            View rowView = null;
-
-            if (sender.equals("me")) {
-                 rowView = inflater.inflate(R.layout.listitem_message_chat_right, null);
-                ((TextView) rowView.findViewById(R.id.listitem_messagechat)).setText(smessage);
-                ((TextView) rowView.findViewById(R.id.listitem_messagechat_date)).setText(sdate);
-            } else if (sender.equals(sname)) {
-                 rowView = inflater.inflate(R.layout.listitem_message_chat_left, null);
-                ((TextView) rowView.findViewById(R.id.listitem_messagechat)).setText(smessage);
-                ((TextView) rowView.findViewById(R.id.listitem_messagechat_date)).setText(sdate);
+        //Was in sendMessage before
+        //This part is to
+        mFirebaseRef.child("-KJpH3kvrIchLgS9IVB7").child("messages").orderByKey().limitToLast(1).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                System.out.println(dataSnapshot.getKey());
+                int keyInt = Integer.parseInt(dataSnapshot.getKey()) + 1;
+                currentKey = String.valueOf(keyInt);
             }
 
-            return rowView;
-        }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        // Setup our input methods. Enter key on the keyboard or pushing the send button
+        EditText inputText = (EditText) findViewById(R.id.messageInput);
+        inputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_NULL && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    sendMessage();
+                }
+                return true;
+            }
+        });
+
+        findViewById(R.id.send_message).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
 
     }
-}*/
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
+        final ListView listView = getListView();
+        // Tell our list adapter that we only want 50 messages at a time
+        //Was used before
+        //mChatListAdapter = new ChatListAdapter(mFirebaseRef.limit(50), this, R.layout.message, mUsername);
+        mChatListAdapter = new ChatListAdapter(mFirebaseRef.child("-KJpH3kvrIchLgS9IVB7").child("messages"), this, R.layout.message, mUsername);
+        listView.setAdapter(mChatListAdapter);
+        mChatListAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(mChatListAdapter.getCount() - 1);
+            }
+        });
+
+        // Finally, a little indication of connection status
+        mConnectedListener = mFirebaseRef.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean connected = (Boolean) dataSnapshot.getValue();
+                if (connected) {
+                    Toast.makeText(Message_Chat.this, "Connected to Firebase", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Message_Chat.this, "Disconnected from Firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                // No-op
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mFirebaseRef.getRoot().child(".info/connected").removeEventListener(mConnectedListener);
+        mChatListAdapter.cleanup();
+    }
+
+    private void setupUsername() {
+        SharedPreferences prefs = getApplication().getSharedPreferences("ChatPrefs", 0);
+        mUsername = prefs.getString("username", null);
+        if (mUsername == null) {
+            Random r = new Random();
+            // Assign a random user name if we don't have one saved.
+            mUsername = "JavaUser" + r.nextInt(100000);
+            prefs.edit().putString("username", mUsername).commit();
+        }
+    }
+
+    private void sendMessage() {
+        EditText inputText = (EditText) findViewById(R.id.messageInput);
+        String input = inputText.getText().toString();
+        if (!input.equals("")) {
+            Message message = new Message(mUsername, "10.06.2016", "12:10",  input);
+            mFirebaseRef.child("-KJpH3kvrIchLgS9IVB7").child("messages").child(currentKey).setValue(message);
+
+            inputText.setText("");
+        }
+    }
+
+}
--- app/src/main/java/de/ticket_match/ticketmatch/entities/Message.java
+++ app/src/main/java/de/ticket_match/ticketmatch/entities/Message.java
@@ -1,19 +1,46 @@
 package de.ticket_match.ticketmatch.entities;
 
-import java.util.Date;
-
 public class Message {
+    private String mAuthor;
+    private String mDate;
     private String mText;
-    private String mSender;
-    private Date mDate;
+    private String mTimestamp;
 
-    public Date getDate() {
+    public Message() {
+
+    }
+
+    public Message(String mAuthor, String mDate, String mTimestamp, String mText) {
+        this.mAuthor = mAuthor;
+        this.mDate = mDate;
+        this.mTimestamp = mTimestamp;
+        this.mText = mText;
+    }
+
+    public String getDate() {
         return mDate;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) {
         mDate = date;
     }
+
+    public String getTimestamp() {
+        return mTimestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.mTimestamp = timestamp;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(String author) {
+        this.mAuthor = author;
+    }
+
 
     public String getText() {
         return mText;
@@ -21,13 +48,5 @@ public class Message {
 
     public void setText(String text) {
         mText = text;
-    }
-
-    public String getSender() {
-        return mSender;
-    }
-
-    public void setSender(String sender) {
-        mSender = sender;
     }
 }
