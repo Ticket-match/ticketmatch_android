d88c8197c5c99e65c14e66b284a3b360e49aae2c
LisaHeim <lisa.heimeroth@gmail.com>
2016-06-21 20:58:47 +0200

    Automated Test for Forgot Password  (ID: 04)

--- app/src/androidTest/java/de/ticket_match/ticketmatch/MainActivityRedirectForgotTest.java
+++ app/src/androidTest/java/de/ticket_match/ticketmatch/MainActivityRedirectForgotTest.java
@@ -12,7 +12,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 /**
  * Created by D060644 on 6/21/2016.
  */
-public class MainActivityRedirectRegisterTest {
+public class MainActivityRedirectForgotTest {
+
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(
@@ -22,13 +23,14 @@ public class MainActivityRedirectRegisterTest {
 
     public void test(){
 
-        onView(withId(R.id.textViewRegister)).perform(click());
+        onView(withId(R.id.textView2)).perform(click());
 
-        //stilltodo: Program a check that register page openes up --> need help with tabhost
-        // (-->Manual/Visual check that register page opens up is possible)
+        //stilltodo: Program a check that forgot password page openes up --> need help with tabhost
+        // (-->Manual/Visual check that forgot password page opens up is possible)
 
 
     }
+
 
 
 }
