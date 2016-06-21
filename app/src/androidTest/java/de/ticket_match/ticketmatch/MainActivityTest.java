d9892980caeb629166e884935184c2196a901087
LisaHeim <lisa.heimeroth@gmail.com>
2016-06-21 16:27:54 +0200

    Automated Test for Login (ID: 01) - without Facebook Login

--- app/src/androidTest/java/de/ticket_match/ticketmatch/MainActivityTest.java
+++ app/src/androidTest/java/de/ticket_match/ticketmatch/MainActivityTest.java
@@ -1,19 +1,14 @@
 package de.ticket_match.ticketmatch;
 
 
-import android.content.ComponentName;
-import android.provider.Settings;
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
-import android.view.View;
+import android.support.test.runner.AndroidJUnit4;
 
-import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -21,22 +16,12 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.runner.AndroidJUnit4;
-
-
-import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.*;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
 
 /**
  * Created by D060644 on 6/10/2016.
  */
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
     //Variables
@@ -45,42 +30,42 @@ public class MainActivityTest {
     String mTestLoginPassword;
 
     @Rule
-
- //   public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
-    //        MainActivity.class);
-    public IntentsTestRule< MainActivity> mActivityRule = new IntentsTestRule<>(
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(
             MainActivity.class);
 
     @Before
     public void initValidString() {
         // Specify a valid string.
 
-        mTestLoginMail = "hm@mail.comm";
-        mTestLoginPassword = "hansmuller";
+        mTestLoginMail = "m@m.com";
+        mTestLoginPassword = "maxmax";
     }
 
     @Test
+    public void changeText() {
 
-    public void changeText_sameActivity() {
-
-        //Type in Email and Password and press Login Button
-
+        //Insert text automatically
         onView(withId(R.id.login_mail))
                 .perform(typeText(mTestLoginMail));
-
         onView(withId(R.id.login_password))
                 .perform(typeText(mTestLoginPassword));
 
+
+        //Check
+        onView(withId(R.id.login_mail))
+                .check(matches(withText(mTestLoginMail)));
+
+        onView(withId(R.id.login_password))
+                .check(matches(withText(mTestLoginPassword)));
+
+        //Push final Login Button to navigate to next view
         onView(withId(R.id.btn_login)).perform(click());
-
-        //Check that MyProfile Activity opens up
-        intended(allOf(
-               hasComponent(MyProfile.class.getName())));
-
-
-
 
 
     }
 
+
+
+
 }
+
