f25381d43afd16a755f252a6481739570eeb4de2
RebekkaSp <rebekka.sperger@gmx.de>
2016-06-29 15:33:33 +0200

    Change Header Titles

--- app/src/main/java/de/ticket_match/ticketmatch/MainActivityTabHost.java
+++ app/src/main/java/de/ticket_match/ticketmatch/MainActivityTabHost.java
@@ -185,35 +185,35 @@ public class MainActivityTabHost extends AppCompatActivity {
             @Override
             public void onTabChanged(String tabId) {
                 if(tabId.equals("myprofile")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("My profile");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("My Profile");
                 } else if(tabId.equals("messages")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("My messages");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("My Messages");
                 } else if(tabId.equals("tickets")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("My tickets");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("My Tickets");
                 } else if(tabId.equals("search")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("Search results");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("Search Results");
                 } else if(tabId.equals("makeadate")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("My dates");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("My Dates");
                 } else if(tabId.equals("myprofile_ratings")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("My ratings");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("My Ratings");
                 } else if(tabId.equals("messages_chat")) {
                     ((TextView)findViewById(R.id.headerTitle)).setText("Chat");
                 } else if(tabId.equals("tickets_newoffer")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("Offer your ticket");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("Offer Your Ticket");
                 } else if(tabId.equals("makeadate_new")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("Create a new date");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("Create A New Date");
                 } else if(tabId.equals("makeadate_search")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("Search for dates");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("Search For Dates");
                 } else if(tabId.equals("makeadate_search_result")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("Searchs results");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("Searchs Results");
                 } else if(tabId.equals("foreign_profile")) {
                     ((TextView)findViewById(R.id.headerTitle)).setText("Profile");
                 } else if(tabId.equals("foreign_profile_ratings")) {
                     ((TextView)findViewById(R.id.headerTitle)).setText("Ratings");
                 } else if (tabId.equals("tickets_search")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("Search for tickets");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("Search For Tickets");
                 } else if (tabId.equals("edit_myprofile")) {
-                    ((TextView)findViewById(R.id.headerTitle)).setText("Edit my profile");
+                    ((TextView)findViewById(R.id.headerTitle)).setText("Edit My Profile");
                 } /*else if (tabId.equals("change_password")) {
                     ((TextView) findViewById(R.id.headerTitle)).setText("Change your password");
                 } */else {
