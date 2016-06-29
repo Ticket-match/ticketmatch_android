3e6b3a1d4bfa3f7697c613c8917637dd2f7a221b
Alexander Linke <alexander.linke@imbar.de>
2016-06-29 19:56:12 +0200

    Edit Ticket Functionality
    Added the funcitonality to edit tickets.

--- app/src/main/java/de/ticket_match/ticketmatch/NewOffer.java
+++ app/src/main/java/de/ticket_match/ticketmatch/NewOffer.java
@@ -3,6 +3,7 @@ package de.ticket_match.ticketmatch;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -20,6 +21,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
@@ -33,21 +35,69 @@ public class NewOffer extends AppCompatActivity {
 
     private Calendar ticketDate;
 
+    EditText mEventName;
+    EditText mEventLoation;
+    EditText mNumberOfTickets;
+    TextView mDate;
+    TextView mTime;
+    EditText mPrice;
+    CheckBox mCheckBoxPrice;
+    Spinner mEventType;
+    Spinner mCurrency;
+
+    ArrayAdapter<CharSequence> adapterEventType;
+    ArrayAdapter<CharSequence> adapterCurrency;
+
+    //Used to edit a ticket - see Offer_Overview, method: btn_ticket_edit
+    Boolean editTicket = false;
+    String ticketKey;
+
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+    public String getTicketKey() {
+        return ticketKey;
+    }
+
+    public void setTicketKey(String ticketKey) {
+        this.ticketKey = ticketKey;
+    }
+
+    public Boolean getEditTicket() {
+        return editTicket;
+    }
+
+    public void setEditTicket(Boolean editTicket) {
+        this.editTicket = editTicket;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_offer);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.event_type, R.layout.support_simple_spinner_dropdown_item);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        ((Spinner)findViewById(R.id.event_type)).setAdapter(adapter);
 
-        ArrayAdapter<CharSequence> adapter_currency = ArrayAdapter.createFromResource(this, R.array.currency, R.layout.support_simple_spinner_dropdown_item);
-        adapter_currency.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        ((Spinner)findViewById(R.id.currency)).setAdapter(adapter_currency);
+        //Assign all Input Fields
+        mEventName = (EditText)findViewById(R.id.eventname);
+        mEventLoation = (EditText)findViewById(R.id.eventlocation);
+        mNumberOfTickets = (EditText)findViewById(R.id.numberoftickets);
+        mDate = (TextView)findViewById(R.id.date);
+        mPrice = (EditText)findViewById(R.id.price);
+        mTime = (TextView)findViewById(R.id.time);
+        mCheckBoxPrice = (CheckBox)findViewById(R.id.checkbox_price);
+        mEventType = (Spinner)findViewById(R.id.event_type);
+        mCurrency = ((Spinner)findViewById(R.id.currency));
+
+        //ArrayAdapter for the Spinner
+        adapterEventType = ArrayAdapter.createFromResource(this, R.array.event_type, R.layout.support_simple_spinner_dropdown_item);
+        adapterEventType.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        mEventType.setAdapter(adapterEventType);
+
+        adapterCurrency = ArrayAdapter.createFromResource(this, R.array.currency, R.layout.support_simple_spinner_dropdown_item);
+        adapterCurrency.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        mCurrency.setAdapter(adapterCurrency);
 
         //Datepicker
-        ((TextView)findViewById(R.id.date)).setOnTouchListener(new View.OnTouchListener() {
+        mDate.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 ((TicketMatch)getApplication()).minimizeKeyboard(v);
@@ -67,7 +117,7 @@ public class NewOffer extends AppCompatActivity {
         });
 
         //Timepicker
-        ((TextView)findViewById(R.id.time)).setOnTouchListener(new View.OnTouchListener(){
+        mTime.setOnTouchListener(new View.OnTouchListener(){
             String time = "";
             @Override
             public boolean onTouch(View v, MotionEvent event){
@@ -103,26 +153,27 @@ public class NewOffer extends AppCompatActivity {
         boolean checked = ((CheckBox)view).isChecked();
 
         if(checked){
-            ((EditText)findViewById(R.id.price)).setVisibility(View.INVISIBLE);
-            ((EditText)findViewById(R.id.price)).setText("0");
-            findViewById(R.id.currency).setVisibility(View.INVISIBLE);
+            mPrice.setVisibility(View.INVISIBLE);
+            mPrice.setText("0");
+            mCurrency.setVisibility(View.INVISIBLE);
         } else{
-            ((EditText)findViewById(R.id.price)).setVisibility(View.VISIBLE);
-            findViewById(R.id.currency).setVisibility(View.VISIBLE);
+            mPrice.setVisibility(View.VISIBLE);
+            mCurrency.setVisibility(View.VISIBLE);
         }
     }
 
     //check if all values are entered, if yes save data in database and delete input fields
     public void btn_newoffer(View view){
-        String eventname = ((EditText)findViewById(R.id.eventname)).getText().toString();
-        String eventlocation = ((EditText)findViewById(R.id.eventlocation)).getText().toString();
-        String numberoftickets = ((EditText)findViewById(R.id.numberoftickets)).getText().toString();
-        String date = ((TextView)findViewById(R.id.date)).getText().toString();
-        String time = ((TextView)findViewById(R.id.time)).getText().toString();
-        String price = ((EditText)findViewById(R.id.price)).getText().toString();
-        boolean free = ((CheckBox)findViewById(R.id.checkbox_price)).isChecked();
-        String type = ((Spinner)findViewById(R.id.event_type)).getSelectedItem().toString();
-        String currency = ((Spinner)findViewById(R.id.currency)).getSelectedItem().toString();
+
+        String eventname = mEventName.getText().toString();
+        String eventlocation = mEventLoation.getText().toString();
+        String numberoftickets = mNumberOfTickets.getText().toString();
+        String date = mDate.getText().toString();
+        String time = mTime.getText().toString();
+        String price = mPrice.getText().toString();
+        boolean free = mCheckBoxPrice.isChecked();
+        String type = mEventType.getSelectedItem().toString();
+        String currency = mCurrency.getSelectedItem().toString();
 
         HashMap<String, String> price_currency = new HashMap<String, String>(0);
         price_currency.put("value", price);
@@ -132,37 +183,66 @@ public class NewOffer extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Please fill out the required information!",Toast.LENGTH_SHORT).show();
         }
         else {
-            // Create ticket in database
-            Ticket newticket = new Ticket(eventname, type, price_currency, numberoftickets, eventlocation, date, FirebaseAuth.getInstance().getCurrentUser().getUid(), time);
-            ((ArrayList<Ticket>)((MainActivityTabHost) getParent()).baseBundle.getSerializable("tickets_offerdetail")).add(newticket);
-            String key = FirebaseDatabase.getInstance().getReference().child("tickets").push().getKey();
-            ((ArrayList<String>)((MainActivityTabHost) getParent()).baseBundle.getSerializable("tickets_offerdetail_keys")).add(key);
-            FirebaseDatabase.getInstance().getReference().child("tickets").child(key).setValue(newticket);
 
-            // delete input fields
-            ((EditText)findViewById(R.id.eventname)).setText("");
-            ((EditText)findViewById(R.id.eventlocation)).setText("");
-            ((EditText)findViewById(R.id.numberoftickets)).setText("");
-            ((TextView)findViewById(R.id.date)).setText("Date");
-            ((EditText)findViewById(R.id.price)).setText("");
-            ((CheckBox)findViewById(R.id.checkbox_price)).setChecked(false);
-            cb_clicked(findViewById(R.id.checkbox_price));
-            ((Spinner)findViewById(R.id.event_type)).setSelection(0);
-            ((Spinner)findViewById(R.id.currency)).setSelection(0);
-            ((TextView)findViewById(R.id.time)).setText("Time");
+                // Create ticket in database
+                Ticket newticket = new Ticket(eventname, type, price_currency, numberoftickets, eventlocation, date, FirebaseAuth.getInstance().getCurrentUser().getUid(), time);
+                ((ArrayList<Ticket>)((MainActivityTabHost) getParent()).baseBundle.getSerializable("tickets_offerdetail")).add(newticket);
+                String key = FirebaseDatabase.getInstance().getReference().child("tickets").push().getKey();
+                ((ArrayList<String>)((MainActivityTabHost) getParent()).baseBundle.getSerializable("tickets_offerdetail_keys")).add(key);
+                FirebaseDatabase.getInstance().getReference().child("tickets").child(key).setValue(newticket);
 
-            // hide keyboard
-            View aview = this.getCurrentFocus();
-            if (aview != null) {
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(aview.getWindowToken(), 0);
+                // delete input fields
+                setEmtpyTicketTextFields();
+
+                // hide keyboard
+                View aview = this.getCurrentFocus();
+                if (aview != null) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(aview.getWindowToken(), 0);
+                }
+
+                Toast.makeText(getApplicationContext(),"Your ticket is registered!",Toast.LENGTH_SHORT).show();
+                ((TabHost)getParent().findViewById(R.id.tabHost)).setCurrentTabByTag("tickets");
+
+            if(getEditTicket()){
+                TabHost tabHost = ((TabHost)getParent().findViewById(R.id.tabHost));
+                Offer_Overview offerOverview = (Offer_Overview) tabHost.getCurrentView().getContext();
+                offerOverview.deleteTicket(ticketKey);
+                this.setEditTicket(false);
+                this.setTicketKey("");
             }
+                //((Offer_Overview.MyTicketAdapter)((HeaderViewListAdapter)((ListView)((TabHost)getParent().findViewById(R.id.tabHost)).getCurrentView().findViewById(R.id.offeroverview_list)).getAdapter()).getWrappedAdapter()).notifyDataSetChanged();
+                //((Offer_Overview.MyTicketAdapter)(((ListView)((TabHost)getParent().findViewById(R.id.tabHost)).getCurrentView().findViewById(R.id.offeroverview_list)).getAdapter())).notifyDataSetChanged();
+                ((((RecyclerView)((TabHost)getParent().findViewById(R.id.tabHost)).getCurrentView().findViewById(R.id.offeroverview_list)).getAdapter())).notifyDataSetChanged();
 
-            Toast.makeText(getApplicationContext(),"Your ticket is registered!",Toast.LENGTH_SHORT).show();
-            ((TabHost)getParent().findViewById(R.id.tabHost)).setCurrentTabByTag("tickets");
-            //((Offer_Overview.MyTicketAdapter)((HeaderViewListAdapter)((ListView)((TabHost)getParent().findViewById(R.id.tabHost)).getCurrentView().findViewById(R.id.offeroverview_list)).getAdapter()).getWrappedAdapter()).notifyDataSetChanged();
-            //((Offer_Overview.MyTicketAdapter)(((ListView)((TabHost)getParent().findViewById(R.id.tabHost)).getCurrentView().findViewById(R.id.offeroverview_list)).getAdapter())).notifyDataSetChanged();
-            ((((RecyclerView)((TabHost)getParent().findViewById(R.id.tabHost)).getCurrentView().findViewById(R.id.offeroverview_list)).getAdapter())).notifyDataSetChanged();
         }
+
+
     }
+
+    /**
+     * Sets all TextFields of the screen to the approprieate values of a ticket
+     * @param ticket Ticket Entity
+     */
+    public void setTicketTextFields(Ticket ticket){
+        mEventName.setText(ticket.getName());
+        mEventLoation.setText(ticket.getLocation());
+        mNumberOfTickets.setText(ticket.getQuantity());
+        mDate.setText(ticket.getDate());
+        mPrice.setText(ticket.getPrice().get("value"));
+        mCheckBoxPrice.setChecked(false);
+        cb_clicked(mCheckBoxPrice);
+        mEventType.setSelection(adapterEventType.getPosition(ticket.getType()));
+        mCurrency.setSelection(adapterCurrency.getPosition(ticket.getPrice().get("currency")));
+        mTime.setText(ticket.getTime());
+    }
+
+    public void setEmtpyTicketTextFields(){
+        HashMap price = new HashMap();
+        price.put("value", "");
+        price.put("curreny", adapterCurrency.getItem(0));
+        Ticket emptyTicket = new Ticket("","",price,"","","Date","","Time");
+        setTicketTextFields(emptyTicket);
+    }
+
 }
