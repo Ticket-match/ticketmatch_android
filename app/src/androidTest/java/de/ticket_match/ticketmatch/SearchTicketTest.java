eff1da8979d59bd6a91772f0df7d583c3385ef13
TicketPedi <petra_nagel@gmx.de>
2016-07-01 11:19:10 +0200

    SearchTicketsTest

--- app/src/androidTest/java/de/ticket_match/ticketmatch/SearchTicketTest.java
+++ app/src/androidTest/java/de/ticket_match/ticketmatch/SearchTicketTest.java
@@ -30,6 +30,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -48,7 +49,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 public class SearchTicketTest{
 
     //Variables
-
+    String tabetest = "tickets";
     String testLocation;
     String testEvent;
     int year;
@@ -58,9 +59,7 @@ public class SearchTicketTest{
 
  @Rule
 
- public ActivityTestRule<Ticket_Search> searchActivityTestRule = new ActivityTestRule<>(Ticket_Search.class);
-  // public ActivityTestRule<MainActivityTabHost> mActivityRule = new ActivityTestRule<MainActivityTabHost>(MainActivityTabHost.class);
-  //public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+ public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
     public void initValidString() {
@@ -69,38 +68,59 @@ public class SearchTicketTest{
         testLocation = "Mannheim";
         testEvent = "Concert";
         year = 2016;
-        month = 8;
-        day = 4;
+        month = 11;
+        day = 21;
     }
 
     @Test
 
     public void test() throws InterruptedException {
 
-//Spinner Value (Event)
+        //Open Ticket Tab
+        Thread.sleep(500);
+        //Open Message Tab
+        onView(withContentDescription(tabetest)).check(matches(isDisplayed()));
+
+        onView(withContentDescription(tabetest)).perform(click());
+        Thread.sleep(800);
+
+        //Check
+
+        onView(withId(R.id.fab_search_ticket)).check(matches(isDisplayed()));
+
+
+
+        //SearchTicket
+        onView(withId(R.id.fab_search_ticket)).perform(click());
+        Thread.sleep(500);
+
+        //Spinner Value (Event)
         onView(withId(R.id.event_type)).perform(click());
         Thread.sleep(500);
         onData(allOf(is(instanceOf(String.class)), is(testEvent))).perform(click());
 
-        onView(withId(R.id.event_type)).check(matches(withSpinnerText(containsString(testEvent))));
-
+        //Add Location
         onView(withId(R.id.eventlocation)).perform(typeText(testLocation)).perform(closeSoftKeyboard());
-        onView(withId(R.id.eventlocation)).check(matches(withText(testLocation)));
 
         // Datepicker Date
+        onView(withId(R.id.date)).perform(click());
+        Thread.sleep(500);
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(year, month, day));
+        onView(withText("OK")).perform(click());
 
-        onView(withId(R.id.date)).check(matches(isDisplayed()));
-       //onView(withId(R.id.date)).check(matches(is(withClassName(Matchers.equalTo(DatePicker.class.getName())))));
-
-       // onView(withId(R.id.date)).perform(click());
-        //Thread.sleep(500);
-       // onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(year, month, day));
-      //  onView(withText("OK")).perform(click());
+        //Check
+        onView(withId(R.id.event_type)).check(matches(withSpinnerText(containsString(testEvent))));
+        onView(withId(R.id.eventlocation)).check(matches(withText(testLocation)));
+        onView(withId(R.id.date)).check(matches(withText(day + "." + month + "." + year)));
 
         //Submit Search - Click is possible
-        onView(withId(R.id.fab_submit_ticket_search)).check(matches(isCompletelyDisplayed()));
         onView(withId(R.id.fab_submit_ticket_search)).check(matches(isClickable()));
+        onView(withId(R.id.fab_submit_ticket_search)).perform(click());
         //  onView(withClassName(Matchers.equalTo(FloatingActionButton.class.getName()))).perform(click());
+
+        Thread.sleep(800);
+        //Check
+        onView(withId(R.id.find_results)).check(matches(isDisplayed()));
 
     }
 
